d5f415839a1809f5ef37157525a8cc5193772556
Dan White <hibernardwhite@gmail.com>
2018-07-13 12:19:19 +0000

    drawn an image

--- script.js
+++ script.js
@@ -6,8 +6,15 @@
 
         var canvas = Raphael(0, 0, 300, 300);
         var rect = canvas.rect(0, 0, 100, 100);
-
         rect.attr('fill', '#f00');
+
+        var circle = canvas.circle(100, 100, 100);
+        circle.attr('fill', '#ff0');
+
+        img = new Image();
+        img.src = 'html5.png';
+
+        canvas.image(img.src, 100, 50, 150, 150);
 
     }, false);
 }(document));
