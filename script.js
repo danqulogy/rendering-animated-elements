cd683069b3810a4b14630bb8efe3927227456081
Dan White <hibernardwhite@gmail.com>
2018-07-13 11:34:42 +0000

    Added a rectngle using the Raphaeljs

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
-;(function($){
-    
-    $.addEventListener("DOMContentLoaded", function(){
+;
+(function($) {
+
+    $.addEventListener("DOMContentLoaded", function() {
         var body = $.getElementsByTagName('body')[0];
 
-        canvas = $.createElement('canvas');
-        c = canvas.getContext('2d');
+        var canvas = Raphael(0, 0, 300, 300);
+        var rect = canvas.rect(0, 0, 100, 100);
 
-        img = new Image();
-        img.src = 'html5.png';
+        rect.attr('fill', '#f00');
 
-        img.addEventListener('load', function(){
-            canvas.width = 300;
-            canvas.height = 300;
-            
-            c.drawImage(img,0,0, 50, 70);
-            body.appendChild(canvas);
-        });
-    },false);
+    }, false);
 }(document));
