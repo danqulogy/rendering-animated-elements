8c6bb020182f24d54ba80768b973ed201575414c
Dan White <hibernardwhite@gmail.com>
2018-07-13 12:45:48 +0000

    cleaned slate

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 ;
 (function($) {
+    // rAF Shim
+    window.requestAnimFrame = (function() {
+        return window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            function(callback) {
+                window.setTimeout(callback, 1000 / 60);
+            };
+    })();
 
     $.addEventListener("DOMContentLoaded", function() {
         var body = $.getElementsByTagName('body')[0];
 
-        var canvas = Raphael(0, 0, 300, 300);
-        var rect = canvas.rect(0, 0, 100, 100);
-        rect.attr('fill', '#f00');
 
-        var circle = canvas.circle(100, 100, 100);
-        circle.attr('fill', '#ff0');
 
-        img = new Image();
-        img.src = 'html5.png';
-
-        canvas.image(img.src, 100, 50, 150, 150);
 
     }, false);
 }(document));
