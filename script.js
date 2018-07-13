79f3698357116fd0b92ea85f3760ccd384dd5bd8
Dan White <hibernardwhite@gmail.com>
2018-07-13 10:38:45 +0000

    Changed the position of the div via the transform property on style

--- script.js
+++ script.js
@@ -8,6 +8,17 @@
         obj.style.height = 100 + 'px';
         obj.style.backgroundColor = '#f00';
 
+        obj.style.position = 'absolute';
+        obj.style.webkitTransform = 'translateX(' + 200 + 'px)';
+        obj.style.mozTransform = 'translateX(' + 200 + 'px)';
+        obj.style.msTransform = 'translateX(' + 200 + 'px)';
+        obj.style.transform = 'translateX(' + 200 + 'px)';
+
+        obj.style.webkitTransform = 'translateY(' + 400 + 'px)';
+        obj.style.mozTransform = 'translateY(' + 400 + 'px)';
+        obj.style.msTransform = 'translateY(' + 400 + 'px)';
+        obj.style.transform = 'translateY(' + 400 + 'px)';
+
         
 
         body.appendChild(obj);
