d490ba7cc38ec501c9b42afdbfb977180fef77a0
Dan White <hibernardwhite@gmail.com>
2018-07-13 10:32:24 +0000

    added DOMContentListener and created a div object and added it to the body

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-s
+;(function($){
+    
+    $.addEventListener("DOMContentLoaded", function(){
+        var body = $.getElementsByTagName('body')[0];
+
+        obj = $.createElement('div');
+        obj.style.width = 100 + 'px';
+        obj.style.height = 100 + 'px';
+        obj.style.backgroundColor = '#f00';
+
+        
+
+        body.appendChild(obj);
+
+    },false);
+}(document));
